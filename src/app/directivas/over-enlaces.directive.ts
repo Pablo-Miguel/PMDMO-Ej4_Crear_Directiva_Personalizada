0b94ef9c1ab2750f0e2f1b25e1ca639d18831499
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2022-11-09 13:50:34 +0100

    V3

--- src/app/directivas/over-enlaces.directive.ts
+++ src/app/directivas/over-enlaces.directive.ts
@@ -1,23 +1,23 @@
 import { UpperCasePipe } from '@angular/common';
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[pmOverEnlaces]'
 })
 export class OverEnlacesDirective {
 
+  @HostBinding('class.upperCaseVerde') private ishovering!: boolean;
+
   cadena: String = "";
 
   constructor(private el: ElementRef) { }
 
   @HostListener('mouseover') onMouseOver(){
-    this.cadena = this.el.nativeElement.innerHTML
-    this.el.nativeElement.innerHTML = this.cadena.toUpperCase();
+    this.ishovering = true;
   }
-
+  
   @HostListener('mouseout') onMouseOut() {
-    this.cadena = this.el.nativeElement.innerHTML
-    this.el.nativeElement.innerHTML = this.cadena.toLowerCase();
+    this.ishovering = false;
   }
 
 }
